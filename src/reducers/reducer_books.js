cffbb27bda2a882ff7a0e94a7dd22504495fbc32
Matt Stuhring <whereyouatmatt@gmail.com>
2016-12-29 12:11:23 -0800

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Harry Pothead' },
+    { title: 'The Light Tower' },
+    { title: 'Eloquent Life' }
+  ]
+}
